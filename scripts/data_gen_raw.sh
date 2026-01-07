0d35f90cf9726c66029b5bc162adf3c62f6c02dd
chenjian <chenjian@dexforce.com>
2026-01-07 10:51:52 +0800

    update

---
+++ scripts/data_gen_raw.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# A single task should contain 20w trajectories.
+# each dataset folder should contain 600 trajectories with the following rule:
+# max_episodes: 30
+# num_envs: 20
+# Total number of trajectories per bash run: LOOP_COUNT * 30 * 20
+# If use 4 process to run the bash script, we can set LOOP_COUNT=80.
+# totol_run should be around 320
+
+
+SYNC_LOOP_COUNT=8  # Set this to the number of parallel processes you want
+ASYNC_LOOP_COUNT=5   # Number of asynchronous runs per sync run
+
+
+for ((i=0; i<SYNC_LOOP_COUNT; i++)); do
+    for ((j=0; j<ASYNC_LOOP_COUNT; j++)); do
+        python -m embodichain.lab.scripts.run_env \
+            --gym_config gym_config_raw.json \
+            --headless --enable_rt --num_envs 20 --gpu_id 1 &
+        sleep 5
+
+    done
+    
+    wait
+done
